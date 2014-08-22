e1ed1f327865771dee2539dff5dff7d385f9e36b
Joshua Johnson <me@josh.land>
2014-08-22 03:28:09 -0700

    Removing feed (for now) for clarity.

--- feed/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from feed import views
-
-
-urlpatterns = patterns('',
-   url(r'^$', views.Index, name='index'),
-)  # noqa
--- feed/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from feed.models import Story, Feed, Word
-
-
-def Index(request):
-    stories = Story.objects.all()
-    return render(request, 'feed/index.html', {'stories': stories})
